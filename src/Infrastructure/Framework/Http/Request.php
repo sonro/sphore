325120f85e6b1f0fc6d6d354fc00f89261c44107
Christopher Morton <sonro@gmx.com>
2021-02-22 16:13:54 +0000

    Fix formating

--- src/Infrastructure/Framework/Http/Request.php
+++ src/Infrastructure/Framework/Http/Request.php
@@ -4,88 +4,91 @@ namespace Sphore\Infrastructure\Framework\Http;
 
 class Request
 {
-	public function __construct(
-		protected array $post,
-		protected string $body,
-		protected array $query,
-		protected array $headers,
-		protected array $cookies,
-		protected array $server,
-		protected string $path,
-		protected string $method,
-	)
-	{}
+    public function __construct(
+        protected array $post,
+        protected string $body,
+        protected array $query,
+        protected array $headers,
+        protected array $cookies,
+        protected array $server,
+        protected string $path,
+        protected string $method,
+    ) {
+    }
 
-	public static function createFromGlobals(): self
-	{
-		$method = $_SERVER["REQUEST_METHOD"];
-		$path = $_SERVER["REQUEST_URI"];
-		$headers = self::createFromGlobals();
+    public static function createFromGlobals(): self
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $path = $_SERVER['REQUEST_URI'];
+        $headers = self::createHeadersFromGlobals();
 
+        $body = match ($method) {
+            'POST', 'PUT', 'PATCH' => file_get_contents('php://input'),
+            default => '',
+        };
 
-		$body = match($method) {
-			"POST", "PUT", "PATCH" => file_get_contents("php://input"),
-			default => "",
-		};
+        return new self(
+            post: $_POST,
+            body: $body,
+            query: $_GET,
+            headers: $headers,
+            cookies: $_COOKIE,
+            server: $_SERVER,
+            path: $path,
+            method: $method
+        );
+    }
 
-		return new self(
-			post: $_POST, 
-			body: $body, 
-			query: $_GET,
-			headers: $headers,
-			cookies: $_COOKIE,
-			server: $_SERVER,
-			path: $path,
-			method: $method
-		);
-	}
-
-	protected static function createHeadersFromGlobals(): array
-	{
-		$headers = [];
-		foreach ($_SERVER as $key => $value) {
+    protected static function createHeadersFromGlobals(): array
+    {
+        $headers = [];
+        foreach ($_SERVER as $key => $value) {
             if (0 === strpos($key, 'HTTP_')) {
                 $headers[substr($key, 5)] = $value;
-            } else if (\in_array($key, ['CONTENT_TYPE', 'CONTENT_LENGTH', 'CONTENT_MD5'], true)) {
+			} elseif (\in_array(
+				$key, 
+				['CONTENT_TYPE', 'CONTENT_LENGTH', 'CONTENT_MD5'],
+				true
+			)) {
                 $headers[$key] = $value;
             }
         }
-		
-		return $headers;
-	}
 
-	public function getPost(): array
-	{
-		return $this->post;
-	}
+        return $headers;
+    }
 
-	public function getBody(): string
-	{
-		return $this->body;
-	}
+    public function getPost(): array
+    {
+        return $this->post;
+    }
 
-	public function getQuery(): array
-	{
-		return $this->query;
-	}
+    public function getBody(): string
+    {
+        return $this->body;
+    }
 
-	public function getHeaders(): array
-	{
-		return $this->headers;
-	}
+    public function getQuery(): array
+    {
+        return $this->query;
+    }
 
-	public function getServer(): array
-	{
-		return $this->server;
-	}
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
 
-	public function getPath(): string
-	{
-		return $this->path;
-	}
+    public function getServer(): array
+    {
+        return $this->server;
+    }
 
-	public function getMethod(): string
-	{
-		return $this->method;
-	}
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
 }
